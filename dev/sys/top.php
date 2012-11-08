142c9be9a83dde2f43a812ae181a4890648dcfbc
kumabuchi <kumabuchi@ai.cs.kobe-u.ac.jp>
2012-11-08 13:35:16 +0900

    top.php fix

--- dev/sys/top.php
+++ dev/sys/top.php
@@ -4,7 +4,7 @@ header("Content-Type: application/json; charset=utf-8");
 require_once("../config.php");
 
 if( isset($_GET["c"]) && isset($servs[$_GET["c"]])  ){
-	exec("ssh -o 'StrictHostKeyChecking no' -i /home/kumabuchi/.ssh/id_dsa kumabuchi@".escapeshellarg($_GET["c"]).".cs.scitec.kobe-u.ac.jp top -b -n1 | sed '1,6d'", $out );
+	exec( $servs[$_GET['c']] . " top -b -n1 | sed '1,6d'", $out );
 	print( json_encode(decodeComm($out)) );
 }
 
@@ -18,7 +18,7 @@ function decodeComm( $commOut ){
 		$arr = preg_split( "/[\s,]+/", $commOut[$j]);
 		$prefix = $arr[0] == "" ? 1 : 0;
 
-		if( $arr[1+$prefix] != "root" && $arr[11+$prefix] != "top" && $arr[11+$prefix] != "sshd" ){
+		if( $arr[0] != null && $arr[1+$prefix] != "root" && $arr[11+$prefix] != "top" && $arr[11+$prefix] != "sshd" ){
 			$retArray["PID"] = $arr[0+$prefix];
 			$retArray["USER"] = $arr[1+$prefix];
 			$retArray["PR"] = $arr[2+$prefix];
