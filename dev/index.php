d0590ea0ef4d17ef5742f48474e2beb48ab3a439
kumabuchi <kumabuchi@ai.cs.kobe-u.ac.jp>
2012-12-20 08:55:51 +0900

    Disk Free Space add

--- dev/index.php
+++ dev/index.php
@@ -52,13 +52,13 @@ require_once("config.php");
 		    <!-- page buttons -->
        		    <div class="btn-toolbar" style="margin-top:20px;text-align:right">
                         <a class="btn btn-primary" href="#"><i class="icon-refresh icon-white"></i> Refresh</a>
- 		        <a class="btn btn-danger" href="#" id="control"><i class="icon-arrow-left icon-white"></i>  NextPage</a>
+ 		        <a class="btn btn-danger" href="#" id="alert-center"><i class="icon-envelope icon-white"></i>  AlertCenter</a>
 	                <div class="btn-group" style="text-align:left;">
 		           <a class="btn btn-success" href="#"><i class="icon-th-large icon-white"></i> Servers</a>
 		           <a class="btn btn-success dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span></a>
 		           <ul class="dropdown-menu">
 		        	  <li><a href="#" id="all-clusters"><i class="icon-th-large"></i> all servers</a></li>
-		        	  <li class="divider"></li>
+		        	  <!--<li class="divider"></li>-->
 				  <?php
 					foreach( $servs as $name => $comm ){
 						print('<li><a href="#" id="'.$name.'"><i class="icon-ok"></i> '.$name.'</a></li>');
@@ -66,17 +66,15 @@ require_once("config.php");
 				  ?>
 		        	  <li class="divider"></li>
 		        	  <li><a href="#" id="history"><i class="icon-time"></i> history</a></li>
-		        	  <li><a href="#" id="alert-center"><i class="icon-envelope"></i> alert center</a></li>
 			  </ul>
 		        </div>
  		        <a class="btn btn-warning" href="#" id="about"><i class="icon-hand-right icon-white"></i> about</a>
 		    </div>
 		</div>
 
-		<div id="slider" class="span12" style="height:700px;">  <!-- SLIDER -->
+		<div id="slider" class="span12" style="height:750px;">  <!-- SLIDER -->
 
-		<div class="slide"> <!-- slide1 -->
-
+		<div class="slide slide-controls" style="display:none;"> <!-- slide1 -->
                 <div id="cpu" class="span12">
                     <!-- CPU PERCENTAGE -->
                     <h3>CPU PERCENTAGE</h3>
@@ -122,11 +120,24 @@ require_once("config.php");
 	 	    </div>
 
                 </div>
-
 		</div>
 
-		<div class="slide"> <!-- slide2 -->
+		<div class="slide slide-controls" style="display:none;"> <!-- slide2 -->
+                <div id="df" class="span12">
+                    <!-- FREE DISK SPACE -->
+                    <h3>FREE DISK SPACE</h3>
+                    <div class="row">
+			<?php
+				$span = count($servs) == 1 ? "span12" : "span6";
+				foreach( $servs as $name => $comm ){
+					print('<div id="'.$name.'-df" class="'.$span.'"><h4>'.$name.'</h4></div>');
+				}
+			?>
+		    </div>
+		</div>
+		</div>
 
+		<div class="slide slide-controls" style="display:none;"> <!-- slide3 -->
                 <div id="iostat1" class="span12">
                     <!-- IO STATUS -->
                     <h3>Input/Output STATUS</h3>
@@ -235,7 +246,7 @@ require_once("config.php");
         <script src="js/jquery.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	<script src="js/jquery.slider.min.js"></script>
+	<script src="js/jquery.slider.custom.js"></script>
         <script src="js/cluster-server-monitor.php"></script>
         <script src="http://code.highcharts.com/highcharts.js"></script>
         <script src="http://code.highcharts.com/modules/exporting.js"></script>
---
+++ dev/sys/df.php
@@ -0,0 +1,45 @@
+<?php
+header("Content-Type: application/json; charset=utf-8");
+
+require_once("../config.php");
+require_once("utility.php");
+require_once("multilib.php");
+
+if( isset($_GET["s"]) ){
+	system($servs[$_GET["s"]]." 'df -Ph;'");
+}else{
+	write_log($_SERVER['REQUEST_URI']);
+	$url_list = array();
+	foreach( $servs as $name => $comm ){
+		$url_list[] = $url."/sys/df.php?s=".$name;
+	}
+	$res = fetch_multi_url($url_list);
+	$index = 0;
+	$clusterArray = "";
+	foreach( $servs as $name => $comm ){
+		$clusterArray[$name] = decodeComm($res[$index]);
+		++$index;
+	}
+	print( json_encode( $clusterArray ) );
+}
+
+function decodeComm( $commOut ){
+	$lines = explode("\n",$commOut);
+	if( !is_Array( $lines ) )
+		return;
+	for( $i=1; $i<count($lines); $i++ ){
+		$arr = preg_split( "/[\s,]+/", $lines[$i]);
+		if( count($arr) >= 5 ){
+			$tmpArray["filesystem"] = $arr[0];
+			$tmpArray["size"] = $arr[1];
+			$tmpArray["used"] = $arr[2];
+			$tmpArray["avail"] = $arr[3];
+			$tmpArray["use%"] = $arr[4];
+			$tmpArray["mount"] = $arr[5];
+			$retArray[] = $tmpArray;
+		}
+	} 
+	return $retArray;
+}	
+
+
